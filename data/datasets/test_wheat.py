90964e574206f6c98f8185d19aa9e382de1463a4
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2020-07-21 09:36:57 +0700

    add resize to 1024

--- data/datasets/test_wheat.py
+++ data/datasets/test_wheat.py
@@ -23,6 +23,7 @@ class test_wheat(Dataset):
         records = self.df[self.df['image_id'] == image_id]
 
         image = cv2.imread(f'{self.image_dir}/{image_id}.jpg', cv2.IMREAD_COLOR)
+        image = cv2.resize(image,(1024,1024))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
         image /= 255.0
 
